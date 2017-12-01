fd4959f6f377483509e2908f6b2984658d4461e8
Bronson Duran <brons@outlook.com>
2017-12-01 15:24:06 -0800

    static table

--- data-table-component.js
+++ data-table-component.js
@@ -20,32 +20,7 @@ const styles = {
     overflow: 'hidden',
     margin: '20px auto 0',
   },
-  propToggleHeader: {
-    margin: '20px auto 10px',
-  },
 };
-
- // let tableRows = {this.props.data.map((packet) => 
-    //   <TableRow
-    //   selectable={true}>
-    //     <TableHeaderColumn> {packet["relative-start-time"]} </TableHeaderColumn>
-    //     <TableHeaderColumn> {packet["dest-ip"]} </TableHeaderColumn>
-    //     <TableHeaderColumn> {packet["route"]} </TableHeaderColumn>
-    //   </TableRow>
-    // )}
-
- 					// <Table selectable={true} multiSelectable={true}>
-      //       <TableHeader>
-      //         <TableRow>
-      //           <TableHeaderColumn>Timestamp</TableHeaderColumn>
-      //           <TableHeaderColumn>Destination IP</TableHeaderColumn>
-      //           <TableHeaderColumn>Route</TableHeaderColumn>
-      //         </TableRow>
-      //       </TableHeader>
-      //       <TableBody>
-      //         {tableRows}
-      //       </TableBody>
-      //     </Table> 
 
 
 export default class DataTable extends Component {
@@ -54,37 +29,16 @@ export default class DataTable extends Component {
     super(props);
    
     this.state = {
-      fixedHeader: true,
-	    fixedFooter: true,
-	    stripedRows: false,
-	    showRowHover: false,
-	    selectable: true,
-	    multiSelectable: false,
-	    enableSelectAll: false,
-	    deselectOnClickaway: true,
-	    showCheckboxes: true,
 	    height: '300px',
 	    networkTraffic: this.props.networkTraffic,
-	    selected: []
     };
   }
-
-  _isSelected(index) {
-    return this.state.selected.indexOf(index) !== -1;
-  };
-
-  _handleRowSelection(selectedRows) {
-    this.setState({
-      selected: selectedRows,
-    });
-  };
 
   render() {
     return (
       <div>
         <Table
           height={this.state.height}
-          onRowSelection={this._handleRowSelection}
         >
           <TableHeader>
             <TableRow>
@@ -99,11 +53,8 @@ export default class DataTable extends Component {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {this.state.networkTraffic.map((packet, index) => 
-				      <TableRow
-				      key={index}
-				      selected={this._isSelected(index)}
-				      >
+            {this.state.networkTraffic.map((packet) => 
+				      <TableRow>
 				        <TableHeaderColumn> {packet["relative-start-time"]} </TableHeaderColumn>
 				        <TableHeaderColumn> {packet["dest-ip"]} </TableHeaderColumn>
 				        <TableHeaderColumn> {packet["route"]} </TableHeaderColumn>
